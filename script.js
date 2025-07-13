e3cc7ed4d60298853e2bc79c0f4082268b1e5dbb
21216ar <161382565+AngusRenugounder@users.noreply.github.com>
2025-07-13 07:08:03 +0000

    Forgot to commit but finished the animations of the "repetition" text, "colour and contrast" text and "size and scale" text and renamed the branch to "Home Page Animations" as ut would be broader and would require less branches for each aspect of the home-page

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const typography_ids = ['bottomOfT_Typography', 'topOfT_Typography', 'Y_typography','bottomOfP_Typography', 'O_Typography', 'G_Typography','R_Typography', 'A_Typography', 'bottomOfP2_Typography', 'topOfP2_Typography', 'H_Typography', 'Y2_Typography', 'underline_Typography'];
-const circle_ids = ['bottomOfT_Typography', 'topOfT_Typography', 'Y_typography','bottomOfP_Typography', 'O_Typography', 'G_Typography','R_Typography', 'A_Typography', 'bottomOfP2_Typography', 'topOfP2_Typography', 'H_Typography', 'Y2_Typography', 'underline_Typography'];
+const typography_ids = ['bottomOfT_Typography', 'topOfT_Typography', 'Y_typography','bottomOfP_Typography', 'topOfP_Typography', 'O_Typography', 'G_Typography','R_Typography', 'A_Typography', 'bottomOfP2_Typography', 'topOfP2_Typography', 'H_Typography', 'Y2_Typography', 'underline_Typography'];
 
 const animationDuration = 0.25;
 
@@ -11,10 +10,43 @@ typography_ids.forEach((id, index) => {
     // Set stroke dash properties initially
     TweenMax.set(path, { strokeDasharray: length, strokeDashoffset: length });
 
-    // Delay each subsequent path by the duration of previous animation
+    // Delay each subsequent path by the dur ation of previous animation
     TweenMax.to(path, animationDuration, {
       strokeDashoffset: 0,
       delay: index * animationDuration // 0 for first, 3s for second
     });
   }
+});
+
+TweenMax.to('#Repitition_1', 1.5, {y:61, opacity:0.4});
+TweenMax.to('#Repitition_2', 1.5, {opacity: 0.65});
+TweenMax.to('#Repitition_3', 1.5, {y:79});
+
+TweenMax.to("#Colour_ColourAndContrast", {
+  fill: "#FFFFFF",
+  stroke: "#FFFFFF",
+  duration: 4,
+  ease: "power1.inOut"
+});
+
+TweenMax.to("#AndContrast_ColourAndContrast", {
+  fill: "#FEC63D",
+  stroke: "#FEC63D",
+  duration: 4,
+  ease: "power1.inOut"
+});
+
+TweenMax.to("#Size_SizeAndScale", {
+  scale: 1.5,
+  duration: 3,
+  y: 230,
+  x: 37
+});
+
+TweenMax.to("#AndScale_SizeAndScale", {
+  duration: 3,
+  y: 248,
+  x: 38,
+  fill: "#FEC63D",
+  stroke: "#FEC63D"
 });
