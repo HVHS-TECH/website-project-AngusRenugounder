4182bd10f90f0690c57a2e399b58c91db9c2a725
21216ar <161382565+AngusRenugounder@users.noreply.github.com>
2025-10-05 10:59:25 +0000

    Finished with the image modals and created new function in the modal.js file and for the animations in the animation.js file for each page that would run on body onload

--- modal.js
+++ modal.js
@@ -1,27 +1,59 @@
-// Get DOM Elements
-const doModal = document.querySelector('#typography-do-modal');
-const dontModal = document.querySelector('#typography-dont-do-modal');
-const doModalBtn = document.querySelector('#do-modal-btn');
-const dontModalBtn = document.querySelector('#dont-modal-btn');
-const closeBtns = document.querySelectorAll('.close');
+function size_and_scale_modals() {
+ // Get DOM Elements
+  const doModal = document.querySelector('#size_and_scale-do-modal');
+  const dontModal = document.querySelector('#size_and_scale-dont-do-modal');
+  const doModalBtn = document.querySelector('#do-modal-btn');
+  const dontModalBtn = document.querySelector('#dont-modal-btn');
+  const closeBtns = document.querySelectorAll('.close');
 
-// Event Listeners
-doModalBtn.addEventListener('click', () => openModal(doModal));
-dontModalBtn.addEventListener('click', () => openModal(dontModal));
-closeBtns.forEach(btn => btn.addEventListener('click', closeAllModals));
-window.addEventListener('click', outsideClick);
+  // Event Listeners
+  doModalBtn.addEventListener('click', () => openModal(doModal));
+  dontModalBtn.addEventListener('click', () => openModal(dontModal));
+  closeBtns.forEach(btn => btn.addEventListener('click', closeAllModals));
+  window.addEventListener('click', outsideClick);
 
-// Functions
-function openModal(modal) {
-  modal.style.display = 'block';
+  // Functions
+  function openModal(modal) {
+    modal.style.display = 'block';
+  }
+
+  function closeAllModals() {
+    doModal.style.display = 'none';
+    dontModal.style.display = 'none';
+  }
+
+  function outsideClick(e) {
+    if (e.target === doModal) doModal.style.display = 'none';
+    if (e.target === dontModal) dontModal.style.display = 'none';
+  }
 }
 
-function closeAllModals() {
-  doModal.style.display = 'none';
-  dontModal.style.display = 'none';
-}
+function typography_page_modals() {
+  // Get DOM Elements
+  const doModal = document.querySelector('#typography-do-modal');
+  const dontModal = document.querySelector('#typography-dont-do-modal');
+  const doModalBtn = document.querySelector('#do-modal-btn');
+  const dontModalBtn = document.querySelector('#dont-modal-btn');
+  const closeBtns = document.querySelectorAll('.close');
 
-function outsideClick(e) {
-  if (e.target === doModal) doModal.style.display = 'none';
-  if (e.target === dontModal) dontModal.style.display = 'none';
+  // Event Listeners
+  doModalBtn.addEventListener('click', () => openModal(doModal));
+  dontModalBtn.addEventListener('click', () => openModal(dontModal));
+  closeBtns.forEach(btn => btn.addEventListener('click', closeAllModals));
+  window.addEventListener('click', outsideClick);
+
+  // Functions
+  function openModal(modal) {
+    modal.style.display = 'block';
+  }
+
+  function closeAllModals() {
+    doModal.style.display = 'none';
+    dontModal.style.display = 'none';
+  }
+
+  function outsideClick(e) {
+    if (e.target === doModal) doModal.style.display = 'none';
+    if (e.target === dontModal) dontModal.style.display = 'none';
+  }
 }
